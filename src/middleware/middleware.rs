a71eca5a76dd527fa4d49b010377a73a6b7aad4f
chisty <mchisty31@gmail.com>
2024-09-09 20:35:09 +0600

    service and transform traits added

--- src/middleware/middleware.rs
+++ src/middleware/middleware.rs
@@ -1,3 +1,4 @@
+use actix_web::cookie::time::error;
 use actix_web::{dev::ServiceRequest, dev::ServiceResponse, Error, HttpMessage};
 use actix_service::{Service, Transform};
 use futures::future::{ok, Ready};
@@ -11,3 +12,51 @@ pub struct RateLimitMiddleware<S>{
     service:Arc<Mutex<S>>,
 }
 
+
+// Implementing the actix-web middleware trait
+impl<S, B> Transform<S, ServiceRequest> for RateLimiter
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = RateLimitMiddleware<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ok(RateLimitMiddleware {
+            service: Arc::new(Mutex::new(service)),
+        })
+    }
+}
+
+
+impl<S, B> Service<ServiceRequest> for RateLimitMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = Pin<Box<dyn futures::Future<Output = Result<Self::Response, Self::Error>>>>;
+
+    fn poll_ready(&self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        let service = self.service.lock().unwrap();
+        service.poll_ready(cx)
+    }
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        // This is where you check rate-limiting logic and call the next service in the chain
+        let fut = self.service.lock().unwrap().call(req);
+
+        Box::pin(async move {
+            let res = fut.await?;
+            // Implement rate-limiting logic here
+            Ok(res)
+        })
+    }
+}
